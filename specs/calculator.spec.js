fef93b5f7d525fb509205a360cf93061cdc484a4
Timofeev Yurii <georgius@ural.ru>
2020-11-08 22:05:02 +0500

    Updated specs

--- specs/calculator.spec.js
+++ specs/calculator.spec.js
@@ -1,7 +1,18 @@
 import calculator from '../src/calculator';
-describe('Calculator', () => {
+describe('Calculator 100% coverage', () => {
   test('it loads without error', () => {
     expect(calculator).toBeDefined();
     expect(typeof calculator).toBe('function');
+  });
+  test.each`
+    a      | op     | b      | expected
+    ${2}   | ${'*'} | ${2}   | ${4}
+    ${2}   | ${'&'} | ${2}   | ${'error'}
+  `('$a $op $b = $expected', ({ a, op, b, expected }) => {
+    if (expected === 'error') {
+      expect(() => calculator(a, op, b)).toThrow();
+    } else {
+      expect(calculator(a, op, b)).toBe(expected);
+    }
   });
 });
