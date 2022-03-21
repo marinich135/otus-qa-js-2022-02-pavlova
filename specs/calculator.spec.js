3d0788b863e0eb08d821db122c90049dac71261d
Marina <marinich135@gmail.com>
2022-03-22 00:06:41 +0300

    Add tests

--- specs/calculator.spec.js
+++ specs/calculator.spec.js
@@ -6,7 +6,25 @@ describe('Calculator 100% coverage', () => {
   });
   test('2 + 2 = 4', () => {
     expect(calculator(2, '+', 2 )).toBe(4)
-  } )
+  } );
+  test('2 - 2 = 0', () => {
+    expect(calculator(2, '-', 2 )).toBe(0)
+  } );
+  test('2 * 2 = 4', () => {
+    expect(calculator(2, '*', 2 )).toBe(4)
+  } );
+  test('2 / 2 = 1', () => {
+    expect(calculator(2, '/', 2 )).toBe(1)
+  } );
+  test('Throw error when operand A is not a number', () => {
+    expect(() => calculator('?', '*', 2 )).toThrow();
+  } );
+  test('Throw error when operand B is not a number', () => {
+    expect(() => calculator(2, '*', '?' )).toThrow();
+  } );
+  test('Throw error when operator is unknown', () => {
+    expect(() => calculator(2, '?', 2 )).toThrow();
+  } );
   //test.each`
   //  a      | op     | b      | expected
   //  ${2}   | ${'*'} | ${2}   | ${4}
